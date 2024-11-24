21d6e0ae261fa62b3a8320deb80374d3722b944d
JoeyHammoth <jamesputradi@gmail.com>
2024-11-24 23:32:52 +0700

    Added some preliminary GUI

---
+++ pdf-concatenator/src/ConcatMenu.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import javax.swing.*;
+
+public class ConcatMenu {
+    private JFrame frame;
+
+
+    public ConcatMenu(JFrame frame) {
+        this.frame = frame;
+        frame.getContentPane().removeAll();
+        frame.repaint();
+    }
+}
--- pdf-concatenator/src/Main.java
+++ pdf-concatenator/src/Main.java
@@ -8,12 +8,27 @@ import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.io.*;
+import javax.swing.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        
-
-
-
+        JFrame frame = new JFrame();
+        JButton concatButton = new JButton("Concatenate");
+        JButton revButton = new JButton("Reverse");
+        JButton mergeButton = new JButton("Even/Odd Merge");
+        JLabel text = new JLabel("Select a PDF action.");
+        concatButton.setBounds(150, 200, 220, 50);
+        revButton.setBounds(150, 150, 220, 50);
+        mergeButton.setBounds(150, 100, 220, 50);
+        text.setBounds(150, 50, 220, 50);
+        frame.add(concatButton);
+        frame.add(revButton);
+        frame.add(mergeButton);
+        frame.add(text);
+        frame.setSize(500, 600);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
